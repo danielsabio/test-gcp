7932496f14d780c4214e932b3ddefa252c7f4050
danielsabio <daniel.glover@sabiogroup.com>
2024-03-28 16:30:42 +0000

    port 8080

--- src/infra/config/index.ts
+++ src/infra/config/index.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const config = z
   .object({
-    PORT: z.coerce.number().default(3000),
+    PORT: z.coerce.number().default(8080),
     NODE_ENV: z.enum(['test', 'development', 'staging', 'production']),
     REDIS_HOST: z.string(),
     REDIS_PORT: z.coerce.number(),
